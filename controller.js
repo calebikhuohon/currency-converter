d37227ac5c16d026ef506d243e927a62ca97fbe6
caleb <calebikhuohon@gmail.com>
2018-06-28 12:39:50 +0100

    fix controller.js

--- controller.js
+++ controller.js
@@ -1,12 +1,32 @@
-if ('serviceWorker' in navigator) {
-    window.addEventListener('load', () => {
-        navigator.serviceWorker.register('/sw.js')
-            .then((registration) => {
-                //Registration was successful
-                console.log(`serviceWorker reg. has a scope: ${registration}`);
-            }, (err) => {
-                //registration failed
-                console.log(`unsuccessful service worker reg; ${err}`);
-            });
+controller.prototype._registerServiceWorker = () => {
+    if(!navigator.serviceWorker) return;
+
+    const indexController = this;
+
+    navigator.serviceWorker.register('/sw,js')
+        .then((reg) => {
+            if(!navigator.serviceWorker.controller) {
+                return;
+            }
+
+            if (reg.installing) {
+                indexController._trackInstalling(reg.installing);
+                return;
+              }
+          
+              reg.addEventListener('updatefound', () => {
+                indexController._trackInstalling(reg.installing);
+              });
+            
+        });
+}
+
+controller.prototype._trackInstalling = function(worker) {
+    var indexController = this;
+    worker.addEventListener('statechange', function() {
+      if (worker.state == 'installed') {
+        indexController._updateReady(worker);
+      }
     });
-};
+  };
+  
