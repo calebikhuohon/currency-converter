14b6d170c6e9d990cff1f376abb98e03e891c406
Caleb Ikhuohon <calebikhuohon@gmail.com>
2018-06-29 10:26:00 +0100

    minor tweak

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ if ('serviceWorker' in navigator) {
     window.addEventListener('load', () => {
     navigator.serviceWorker.register('/sw.js')
     .then(reg => console.log(`Registration successful`))
-    .catch((err) => console.log("Registration failed" + err));
+  
     });
   }
 
