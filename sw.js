2ac34f567fb47ce736051111377bc52bf5d78d80
caleb <calebikhuohon@gmail.com>
2018-06-30 14:39:17 +0100

    minor fixes

--- sw.js
+++ sw.js
@@ -28,10 +28,7 @@ self.addEventListener("fetch", event => {
  
   event.respondWith(
     caches.match(event.request).then(response => {
-      return response || fetch(event.request).then(function(response) {
-        cache.put(event.request, response.clone());
-        return response;
-      })
+      return response || fetch(event.request);
     })
   );
 });
