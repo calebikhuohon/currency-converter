b857ac6e62b5b98036f713a5d7f2de6c64d6e0ba
caleb <calebikhuohon@gmail.com>
2018-06-30 12:28:46 +0100

    minor fixes

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 const staticCacheName = `converter-static-v2`;
 
-const URLs = [
+const filesToCache = [
   './',
   './index.js',
   './index.css',
@@ -14,13 +14,42 @@ self.addEventListener('install', (event) => {
   event.waitUntil(
     caches.open(staticCacheName).then((cache) => {
       console.log('[ServiceWorker] Caching cacheFiles');
-      return cache.addAll(URLs);
+      return cache.addAll(filesToCache);
     }).then(() => self.skipWaiting())
     .catch(err => console.log('error occured while caching files'))
   );
 });
 
+self.addEventListener('fetch', (event) => {
+  console.log("[ServiceWorker] Fetching", e.request.url)
+  var requestUrl = new URL(event.request.url);
 
+
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+
+      let requestClone = e.request.clone();
+
+      fetch(requestClone).then((response) => {
+        if (!response) {
+         console.log('[ServiceWorker] No response from fetch');
+          return response;
+         }
+
+        let responseClone = response.clone();
+
+         caches.open(staticCacheName).then((cache) => {
+           cache.put(e.request, responseClone);
+           return fetch(e.request);
+        });
+
+       }).catch((err) => {
+        console.log('[ServiceWorker] Error fetching & caching');
+       });
+    })
+  );
+});
 self.addEventListener('activate', (event) => {
   console.log("[ServiceWorker] Activated");
   event.waitUntil(
@@ -37,33 +66,3 @@ self.addEventListener('activate', (event) => {
   );
 });
 
-self.addEventListener('fetch', (event) => {
-  console.log("[ServiceWorker] Fetching", e.request.url)
-  var requestUrl = new URL(event.request.url);
-
-
-  event.respondWith(
-    caches.match(event.request).then((response) => {
-      return response || fetch(event.request);
-
-      let requestClone = e.request.clone();
-
-      // fetch(requestClone).then((response) => {
-      //   if (!response) {
-      //     console.log('[ServiceWorker] No response from fetch');
-      //     return response;
-      //   }
-
-      //   let responseClone = response.clone();
-
-      //   caches.open(staticCacheName).then((cache) => {
-      //     cache.put(e.request, responseClone);
-      //     return fetch(e.request);
-      //   });
-
-      // }).catch((err) => {
-      //   console.log('[ServiceWorker] Error fetching & caching');
-      // });
-    })
-  );
-});
