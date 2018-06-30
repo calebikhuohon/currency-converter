3e9fcb00eac3c6f5d370e8bdab7b8e38363719a1
caleb <calebikhuohon@gmail.com>
2018-06-30 12:22:59 +0100

    minor fixes

--- sw.js
+++ sw.js
@@ -1,68 +1,69 @@
 const staticCacheName = `converter-static-v2`;
 
 const URLs = [
-            './',
-          './index.js',
-          './index.css',
-          './index.html',
-          
-        ];
+  './',
+  './index.js',
+  './index.css',
+  './index.html',
+  './manifest.json',
+  './idb.js'
+];
 
 self.addEventListener('install', (event) => {
-            console.log("[ServiceWorker] installed");
-    event.waitUntil(
-      caches.open(staticCacheName).then((cache) => {
-                   console.log('[ServiceWorker] Caching cacheFiles');
-        return cache.addAll(URLs);
-      })
-    );
-  });
-  
-  
-  self.addEventListener('activate', (event) => {
-              console.log("[ServiceWorker] Activated");
-    event.waitUntil(
-      caches.keys().then((cacheNames) => {
-        return Promise.all(
-          cacheNames.filter((cacheName) => {
-            return cacheName.startsWith('converter-') &&
-                   cacheName != staticCacheName;
-          }).map((cacheName) => {
-            return caches.delete(cacheName);
-          })
-        );
-      })
-    );
-  });
+  console.log("[ServiceWorker] installed");
+  event.waitUntil(
+    caches.open(staticCacheName).then((cache) => {
+      console.log('[ServiceWorker] Caching cacheFiles');
+      return cache.addAll(URLs);
+    }).then(() => self.skipWaiting())
+    .catch(err => console.log('error occured while caching files'))
+  );
+});
 
-  self.addEventListener('fetch',(event) => {
-              console.log("[ServiceWorker] Fetching", e.request.url)
-    var requestUrl = new URL(event.request.url);
-  
- 
-    event.respondWith(
-      caches.match(event.request).then((response) => {
-        return response || fetch(event.request);
-                  
-                    let requestClone = e.request.clone();
 
-            fetch(requestClone).then((response) => {
-                if (!response) {
-                    console.log('[ServiceWorker] No response from fetch');
-                    return response;
-                }
+self.addEventListener('activate', (event) => {
+  console.log("[ServiceWorker] Activated");
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.filter((cacheName) => {
+          return cacheName.startsWith('converter-') &&
+            cacheName != staticCacheName;
+        }).map((cacheName) => {
+          return caches.delete(cacheName);
+        })
+      );
+    })
+  );
+});
 
-                let responseClone = response.clone();
+self.addEventListener('fetch', (event) => {
+  console.log("[ServiceWorker] Fetching", e.request.url)
+  var requestUrl = new URL(event.request.url);
 
-                caches.open(staticCacheName).then((cache) => {
-                    cache.put(e.request, responseClone);
-                    return fetch(e.request);
-                });
 
-            }).catch((err) => {
-                console.log('[ServiceWorker] Error fetching & caching');
-            });
-      })
-    );
-  });
-  
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+
+      let requestClone = e.request.clone();
+
+      // fetch(requestClone).then((response) => {
+      //   if (!response) {
+      //     console.log('[ServiceWorker] No response from fetch');
+      //     return response;
+      //   }
+
+      //   let responseClone = response.clone();
+
+      //   caches.open(staticCacheName).then((cache) => {
+      //     cache.put(e.request, responseClone);
+      //     return fetch(e.request);
+      //   });
+
+      // }).catch((err) => {
+      //   console.log('[ServiceWorker] Error fetching & caching');
+      // });
+    })
+  );
+});
