a64dca750e0e45adc25284f0b00cda79ff2adcb3
caleb <calebikhuohon@gmail.com>
2018-06-30 12:33:08 +0100

    minor fixes

--- sw.js
+++ sw.js
@@ -28,41 +28,20 @@ self.addEventListener('fetch', (event) => {
   event.respondWith(
     caches.match(event.request).then((response) => {
       return response || fetch(event.request);
-
-      let requestClone = e.request.clone();
-
-      fetch(requestClone).then((response) => {
-        if (!response) {
-         console.log('[ServiceWorker] No response from fetch');
-          return response;
-         }
-
-        let responseClone = response.clone();
-
-         caches.open(staticCacheName).then((cache) => {
-           cache.put(e.request, responseClone);
-           return fetch(e.request);
-        });
-
-       }).catch((err) => {
-        console.log('[ServiceWorker] Error fetching & caching');
-       });
     })
   );
 });
-self.addEventListener('activate', (event) => {
-  console.log("[ServiceWorker] Activated");
+self.addEventListener("activate", event => {
   event.waitUntil(
-    caches.keys().then((cacheNames) => {
-      return Promise.all(
-        cacheNames.filter((cacheName) => {
-          return cacheName.startsWith('converter-') &&
-            cacheName != staticCacheName;
-        }).map((cacheName) => {
-          return caches.delete(cacheName);
+    caches.keys().then(keyList => {
+      Promise.all(
+        keyList.map(key => {
+          if (key !== staticCacheName) {
+            caches.delete(key);
+            console.log(`deleted ${key}`)
+          }
         })
       );
     })
   );
 });
-
